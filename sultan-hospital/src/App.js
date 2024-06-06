f8d6acae42bf9c7cab493deabbaee3a1420be7ed
ilix-studio <ilishjyoti17@gmail.com>
2024-06-06 23:28:42 +0530

    Navbar created and responsive

---
+++ sultan-hospital/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar/Navbar";
+import GlobalStyles from "./Components/Styles/GLobal";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
