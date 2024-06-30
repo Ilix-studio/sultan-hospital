98f0442282d9d8f73ac178956cfb56419e2ed3d3
ilix-studio <ilishjyoti17@gmail.com>
2024-07-01 03:09:44 +0530

    MadeFooter

--- client/src/Components/Box3/Box3.jsx
+++ client/src/Components/Box3/Box3.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Box3.css'
+import React from "react";
+import "./Box3.css";
 function Box3() {
   return (
-    <div className='container3'>
-        <div className='box-item3'></div>
-        <div className='box-item4'></div>
+    <div className="container3">
+      <div className="box-item3"></div>
+      <div className="box-item4"></div>
     </div>
-  )
+  );
 }
 
-export default Box3
+export default Box3;
--- client/src/Components/Layouts/WithoutNavbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WithoutNavbar = ({ children }) => {
-  return <main>{children}</main>;
-};
-
-export default WithoutNavbar;
