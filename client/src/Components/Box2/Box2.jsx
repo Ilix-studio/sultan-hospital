d57fee2c4d05048d8d582fca853bb8dd2fd29213
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-07-01 10:17:39 +0530

    Box1 and Box2 edited

--- client/src/Components/Box2/Box2.jsx
+++ client/src/Components/Box2/Box2.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import './Box2.css'
-function Box2() {
-  return (
-    <div className='box-container'>
-        <h1 >DEPARTMENTS</h1>
-        <div className='box-item'>NEUROLOGY</div>
-        <div className='box-item'>CARDIOLOGY</div>
-        <div className='box-item'>UROLOGY</div>
-        <div className='box-item'>ORTHOPEDICS</div>
-        <div className='box-item'>GYNEOLOGY</div>
-        <div className='box-item'>MEDICINE</div>
-        <div className='box-item'>PATHOLOGY</div>
-        <div className='box-item'>PEDIATICS</div>
-        <div className='box-item'>ENDOCRINOLOGY</div>
-        <div className='box-item'>ANESTHESIOLOGY</div>
-        <div className='box-item'>SURGERY</div>
-        <div className='box-item'>PSYSIOTHERAPY</div>
-        
-    </div>
-  )
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Box2.css';
 
-export default Box2
+const Box2 = () => {
+  const navigate = useNavigate();
+
+  const departments = [
+        'NEUROLOGY',
+        'CARDIOLOGY',
+        'UROLOGY',
+        'ORTHOPEDICS',
+        'GYNECOLOGY',
+        'MEDICINE',
+        'PATHOLOGY',
+        'PEDIATRICS',
+        'ENDOCRINOLOGY',
+        'ANESTHESIOLOGY',
+        'SURGERY',
+        'PHYSIOTHERAPY',
+      ];
+
+  const handleDepartmentClick = (department) => {
+    navigate(`/doctors/${department}`);
+  };
+
+  return (
+    <div className="container">
+       <div className="title-row">
+        <h1 className="title">DEPARTMENTS</h1>
+      </div>
+     {departments.map((department, index) => (
+        <div 
+          className="department-row" 
+           key={index} 
+           onClick={() => handleDepartmentClick(department)}
+        >
+         <div className="department-background"/>
+          <div className="department-name">{department}</div>
+         </div>
+     ))}
+   </div>
+  );
+};
+
+export default Box2;
+
+
+
