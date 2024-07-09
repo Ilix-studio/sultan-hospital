1cb49a4bde5797d5391e855e2f0a43684f305c70
AmlanDihingia <dihingiaamlan@gmail.com>
2024-07-09 19:01:21 +0530

    New Changes

--- client/src/Components/Box/Box.jsx
+++ client/src/Components/Box/Box.jsx
@@ -4,7 +4,14 @@ import './Box.css';
 function Box() {
   return (
     <>
-    
+     <div className='grand_parent'>
+      <div className='parents'>
+        <div className='child'>Box1</div>
+        <div className='child'>Box2</div>
+        <div className='child'>Box3</div>
+        <div className='child'>Box4</div>
+      </div>
+     </div>
     
     </>
   )
