07eefc29b9a176146297c210858c18ad9eb4634b
Ilish jyotish <40943217+Ilix-studio@users.noreply.github.com>
2024-07-23 18:04:55 +0530

    "refactor: code optimize"

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -5,9 +5,6 @@ import Heading from './Heading/Heading'
 import Navbar from './Navbar/Navbar'
 // import Cards from "./CreateAppointment/Cards"
 import Footer from './Footer/Footer'
-import AppForm from './Appointment/appForm'
-
-import Footer from './Footer/Footer'
 
 const Home = () => {
   return (
@@ -15,7 +12,7 @@ const Home = () => {
       <Navbar />
       <Heading />
       {/* <Cards /> */}
-      <AppForm />
+
       <HealthServices />
       <Departments />
       <BottomContainer />
